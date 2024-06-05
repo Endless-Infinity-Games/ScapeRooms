e71350c9915c8c8c060a1f355a7e4c21ef245e81
u1990700 <160511681+u1990700@users.noreply.github.com>
2024-06-05 23:26:04 +0200

    Update menus.js

--- JS/menus.js
+++ JS/menus.js
@@ -1,7 +1,12 @@
 
 // Lógica del menú
-document.getElementById('start-game').addEventListener('click', () => {
+/*document.getElementById('start-game').addEventListener('click', () => {
 
     window.location.assign("./html/phasergame.html");
-});
+});*/
 
+document.getElementById('play').addEventListener('click', 
+    function(){
+        sessionStorage.removeItem("save");
+        window.location.assign("./html/phasergame.html");
+    });
