fe7ecd5fb62a2d0d4c8fe3f49d865d872c831b6f
RobertLope <u1990700@campus.udg.edu>
2024-06-05 23:42:35 +0200

    Logo menu
    Transformat el logo html a svg

--- JS/menus.js
+++ JS/menus.js
@@ -1,7 +1,24 @@
 
 // Lógica del menú
-document.getElementById('start-game').addEventListener('click', () => {
+document.getElementById('start-game').addEventListener('click', 
+    function(){
+        window.location.assign("./html/phasergame.html");
+    });
 
-    window.location.assign("../html/phasergame.html");
+document.addEventListener('DOMContentLoaded', function () {
+    var logoImage = document.getElementById('logo-image');
+    
+    logoImage.addEventListener('load', function () {
+        var triggerArea = logoImage.contentDocument.getElementById('triggerArea');
+        var hiddenArea = logoImage.contentDocument.getElementById('hiddenArea');
+        
+        triggerArea.addEventListener('mouseover', function () {
+            hiddenArea.style.visibility = 'visible';
+        });
+        
+        triggerArea.addEventListener('mouseout', function () {
+            hiddenArea.style.visibility = 'hidden';
+        });
+    });
 });
 
